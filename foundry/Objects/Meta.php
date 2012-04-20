6e1445d75332719dd04df36418cfe9f5d14af349
Col <cmacdonald.au@gmail.com>
2012-04-21 01:43:00 +1000

    removing dodgy workaround

--- foundry/Objects/Meta.php
+++ foundry/Objects/Meta.php
@@ -10,7 +10,7 @@ class Meta extends core\BaseObject
     const DATASTORE      = '{"type":"mysql","table":"meta"}';
     const DATASTORE_TYPE = core\Datastore::TYPE_CORE;
     
-//    protected $cacheable = true;
+    protected $cacheable = true;
 
     public static $dataFields = array(
         'id'    => 'int',
